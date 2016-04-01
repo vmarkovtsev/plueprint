4ccf609b209e73a275725bf9be68d0ed9ca96e8e
Vadim Markovtsev <v.markovtsev@samsung.com>
2016-04-01 23:18:27 +0300

    Bump version to 0.4.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name="plueprint",
     description="API Blueprint (https://apiblueprint.org/) parser in pure "
                 "Python",
-    version="0.4.1",
+    version="0.4.2",
     license="New BSD",
     author="Vadim Markovtsev",
     author_email="gmarkhor@gmail.com",
